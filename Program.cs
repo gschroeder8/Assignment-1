360db3c2903b35fe335ac0f1e7deb381ed3999db
gschroeder8 <gschroeder8@my.wctc.edu>
2024-02-01 14:19:57 -0600

    first commit

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿string file = "Tickets.txt";
+
+Console.WriteLine("Do you want to enter a ticket? (Y/N)");
+string addQuestion = Console.ReadLine().ToUpper();
+
+if (addQuestion == "Y")
+{
+    Console.WriteLine("Enter Ticket ID:");
+    string? ticketId = Console.ReadLine();
+
+    Console.WriteLine("Enter Summary:");
+    string? summary = Console.ReadLine();
+
+    Console.WriteLine("Enter Status:");
+    string? status = Console.ReadLine();
+
+    Console.WriteLine("Enter Priority:");
+    string? priority = Console.ReadLine();
+
+    Console.WriteLine("Enter Submitter:");
+    string? submitter = Console.ReadLine();
+
+    Console.WriteLine("Enter Assigned:");
+    string? assigned = Console.ReadLine();
+
+    Console.WriteLine("Enter Watching:");
+    string? watching = Console.ReadLine();
+
+    string csvOutput = $"\n{ticketId},{summary},{status},{priority},{submitter},{assigned},{watching}";
+
+    //appending the newly added data to the CSV file
+    //https://www.tutorialspoint.com/chash-program-to-append-text-to-an-existing-file
+    using (StreamWriter sw = File.AppendText(file))
+    {
+        sw.WriteLine(csvOutput);
+    }
+
+    Console.WriteLine("Data added to the .txt file.");
+}
+else
+{
+    Console.WriteLine("Application ended.");
+}
