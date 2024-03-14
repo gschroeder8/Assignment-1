90c9741b8c90c17ccd1cf3f684e88f8b0d3dc7fa
Gavin Schroeder <gschroeder8@my.wctc.edu>
2024-03-13 23:15:20 -0500

    Adding getters and setters/ New Ticket class

---
+++ Ticket.cs
@@ -0,0 +1,11 @@
+class Ticket
+{
+    public string TicketId { get; set; }
+    public string Summary { get; set; }
+    public string Status { get; set; }
+    public string Priority { get; set; }
+    public string Submitter { get; set; }
+    public string Assigned { get; set; }
+    public string Watching { get; set; }
+    
+}
